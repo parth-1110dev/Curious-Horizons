d3d9168212b9eef2ea854f3a82359b66cc058c59
Parth-1110 <parthnargunde1110@gmail.com>
2026-05-15 15:54:35 +0530

    Configure CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -15,9 +15,15 @@ load_dotenv()
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://lockedin-ai-ten.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
